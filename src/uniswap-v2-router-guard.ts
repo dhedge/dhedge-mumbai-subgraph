d23dc50384cee12f3224bd9bce80e26506830e3a
hedgedon <donpark@pm.me>
2021-06-10 19:19:01 -0500

    feat(UniswapV2RouterGuard): adds new UniswapV2RouterGuard as static data source

---
+++ src/uniswap-v2-router-guard.ts
@@ -0,0 +1,19 @@
+import {
+  Exchange as ExchangeEvent,
+} from '../generated/UniswapV2RouterGuard/UniswapV2RouterGuard';
+import {
+  Exchange,
+} from '../generated/schema';
+import { dataSource, log } from '@graphprotocol/graph-ts';
+
+export function handleExchange(event: ExchangeEvent): void {
+  let entity = new Exchange(
+    event.transaction.hash.toHex() + '-' + event.logIndex.toString()
+  );
+  entity.fundAddress = event.params.fundAddress;
+  entity.sourceAsset = event.params.sourceAsset;
+  entity.sourceAmount = event.params.sourceAmount;
+  entity.destinationAddress = event.params.destinationAddress;
+  entity.time = event.params.time;
+  entity.save();
+}
