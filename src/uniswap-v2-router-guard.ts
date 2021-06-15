7e26856262dd8dbba4e9e87820588dfffbe6e84b
hedgedon <donpark@pm.me>
2021-06-15 12:36:27 -0500

    establishes relationship between Pool and Exchanges entity

--- src/uniswap-v2-router-guard.ts
+++ src/uniswap-v2-router-guard.ts
@@ -14,12 +14,34 @@ export function handleExchange(event: ExchangeEvent): void {
   let entity = new Exchange(
     event.transaction.hash.toHex() + '-' + event.logIndex.toString()
   );
-  let contract = PoolLogic.bind(event.address);
 
-  let id = dataSource.address().toHexString();
-  let pool = Pool.load(id);
+  // log.info(
+  //   'exchange entity in tx hash: {} at blockNumber: {}', 
+  //   [event.transaction.hash.toHex(), event.block.number.toString()]
+  // );
+
+  // let contract = PoolLogic.bind(event.address);
+  let contract = PoolLogic.bind(event.params.fundAddress);
+
+  // dataSource.address() will give you the address of the contract that datasource is listening to.
+  // try with: event.params.fundAddress
+  // let id = dataSource.address().toHexString();
+  // log.info(
+  //   'exchange entity logging dataSource.address from dataSource: {}',
+  //   [id]
+  // );
+
+  // let pool = Pool.load(id);
+  let pool = Pool.load(event.params.fundAddress.toHexString());
+
+  // log.info(
+  //   'logging pool entity id: {}',
+  //   [pool.id]
+  // )
+
   if (!pool) {
-    pool = new Pool(id);
+    // pool = new Pool(id);
+    pool = new Pool(event.params.fundAddress.toHexString());
     pool.fundAddress = event.params.fundAddress;
   }
   let tryPoolName = contract.try_name()
